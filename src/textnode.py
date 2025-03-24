90bb7c39c89329e7e1c645dc4dca8d62044eac49
wfcornelissen <wfcornelissen@gmail.com>
2025-03-24 08:19:40 +0200

    Fixed after merge

--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from src.htmlnode import *
+
 
 class TextType(Enum):
     TEXT = "text"
@@ -22,21 +22,7 @@ class TextNode():
     def __repr__(self):
         return f"TextNode({self.text},{self.text_type},{self.url})"
     
-def text_node_to_html_node(text_node):
-    if text_node.text_type not in TextType:
-        raise Exception("Not a valid text type")
-    if text_node.text_type == TextType.TEXT:
-        return LeafNode(None, text_node.text)
-    if text_node.text_type == TextType.BOLD:
-        return LeafNode("b", text_node.text)
-    if text_node.text_type == TextType.ITALIC:
-        return LeafNode("i", text_node.text)
-    if text_node.text_type == TextType.CODE:
-        return LeafNode("code", text_node.text)
-    if text_node.text_type == TextType.LINKS:
-        return LeafNode("a", text_node.text,{"href":text_node.url})
-    if text_node.text_type == TextType.IMAGE:
-        return LeafNode("img", "",{"src":text_node.url,"alt":text_node.text})
+
         
 
         
