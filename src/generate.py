ae24c1dfd9ea6dffa30fd85077029728c2f87fe2
Werich Cornelissen <werichcornelissen@Werichs-Air.lan>
2025-03-24 20:28:57 +0200

    Changed main to use new recursive func from generate.py

--- src/generate.py
+++ src/generate.py
@@ -1,12 +1,12 @@
 import os
 from extract_markdown import *
 from blocks import *
+import shutil
 
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using template {template_path}")
     if os.path.exists(dest_path):
         os.remove(dest_path)
-
 
     contents = open(from_path, "r").read()
     template = open(template_path, "r").read()
@@ -23,3 +23,18 @@ def generate_page(from_path, template_path, dest_path):
         open(dest_path, "w").write(template)
 
     pass
+
+def generate_pages_recursive(src=None):
+    if src == None:
+        src = "content"
+    dest = "public"
+    template = "template.html"
+
+    for dir in os.listdir(src):
+        if os.path.isfile(src + "/" + dir):
+            print(f"File: {src + '/' + dir}")
+            generate_page(src + "/" + dir, template, src.replace("content", "public") + "/" + dir.replace(".md", ".html"))
+        else:
+            print(f"Dir: {dir}")
+            generate_pages_recursive(src + "/" + dir)
+            
--- src/main.py
+++ src/main.py
@@ -5,12 +5,7 @@ from generate import *
 
 def main():
     explore_src("static", "public", [])
-    generate_page("content/index.md", "template.html", "public/index.html")
-    generate_page("content/contact/index.md", "template.html", "public/contact/index.html")
-    generate_page("content/blog/glorfindel/index.md", "template.html", "public/blog/glorfindel/index.html")
-    generate_page("content/blog/tom/index.md", "template.html", "public/blog/tom/index.html")
-    generate_page("content/blog/majesty/index.md", "template.html", "public/blog/majesty/index.html")
-    
+    generate_pages_recursive()
 
 
 main()
